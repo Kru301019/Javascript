e7473588fe9ff53acbb702e4ee7f713699c12ca9
Kru301019 <krujoshi3010@gmail.com>
2024-03-04 01:52:26 +0000

    ++

--- week 3/index.js
+++ week 3/index.js
@@ -15,24 +15,47 @@
 
 // tttcAttacher();
 
-function lovelyToggle() {
-    // Get the paragraph element
-    var para = document.getElementById('thisisalovelyparagraph');
+// function lovelyToggle() {
+//     // Get the paragraph element
+//     var para = document.getElementById('thisisalovelyparagraph');
 
-    // Check the current color of the paragraph text
-    if (para.style.color == 'red') {
-        // If the color is currently red, change it to blue
-        para.style.color = 'blue';
-    } else {
-        // If the color is not currently red (including if it's not set at all), change it to red
-        para.style.color = 'red';
-    }
+//     // Check the current color of the paragraph text
+//     if (para.style.color == 'red') {
+//         // If the color is currently red, change it to blue
+//         para.style.color = 'blue';
+//     } else {
+//         // If the color is not currently red (including if it's not set at all), change it to red
+//         para.style.color = 'red';
+//     }
+// }
+
+
+// function lovelyParaAttacher() {
+    
+//     const button_1 = document.querySelector('#thisisalovelyparagraph');
+//     button_1.addEventListener('click', lovelyToggle);
+// };
+
+const inputOne = document.querySelector("#in1");
+const inputTwo = document.querySelector("#in2");
+const out = document.querySelector("#out1");
+
+function updateOutPut() {
+
+  const combainedValue = `${inputOne.value} ${inputTwo.value}`;
+  out.textContent = combainedValue;
 }
 
+function checkForEnter(e) {
+  if (e.key === "Enter") {
+    updateOutPut();
+  }
+}
 
-function lovelyParaAttacher() {
-    
-    const button_1 = document.querySelector('#button1');
-    button_1.addEventListener('click', lovelyToggle);
-};
+function concatAttacher() {
+  
+  inputOne.addEventListener("keydown", checkForEnter);
+  inputTwo.addEventListener("keydown", checkForEnter);
+}
 
+concatAttacher();
