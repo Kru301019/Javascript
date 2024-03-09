d4e48a77c6802d3d0253d3336dcf7293c805d42c
Kru301019 <krujoshi3010@gmail.com>
2024-03-09 20:18:18 +0000

    +++

--- week 1/test.js
+++ week 1/test.js
@@ -1,164 +1,510 @@
-const prompt = require("prompt-sync")();
+/* eslint-disable no-undef */
+'use strict';
+QUnit.config.reorder = false;
+const { test } = QUnit;
 
-function CustomPrompt() {
-  let userInput = prompt("Enter something:");
-  console.log("You entered: " + userInput);
+// turns an array-like into an array
+function arr(a) {
+  return Array.prototype.slice.call(a);
 }
 
-function addNumber(a, b) {
-  return a + b;
-}
+test(
+  'Create a function replaceText which accepts two parameters: an element and a string. The function should set the content of the given element to the given string.',
+  function (assert) {
+    if (!assert.functionExists('replaceText', ['elem', 'str'])) return;
 
-let num1 = 5;
-let result = addNumber(num1, 6);
-//console.log("The result is " + result);
+    const replaceThis = document.querySelector('#replacethis');
 
-function subtract(a, b) {
-  return b - a;
-}
+    assert.strictEqual(
+      replaceThis.textContent,
+      '𝔯𝔢𝔭𝔩𝔞𝔠𝔢 𝔱𝔥𝔦𝔰',
+    );
 
-//console.log(`substract is: ${subtract(5,4)}`)
+    replaceText(replaceThis, 'The text has been replaced.');
 
-function checkObject(obj) {
-  if (obj == "obj") {
-    return true;
-  } else {
-    return false;
-  }
-}
+    assert.strictEqual(
+      replaceThis.textContent,
+      'The text has been replaced.',
+    );
 
-//console.log(checkObject('obj'));
+    replaceText(replaceThis, 'The text has been replaced twice.');
 
-function arraySet(arr, i, n) {
-  if (arr[i] == n) {
-    arr[i + 1] = n;
-  } else {
-    arr[i] = n;
-  }
-}
+    assert.strictEqual(
+      replaceThis.textContent,
+      'The text has been replaced twice.',
+    );
+  },
+);
 
-let arr = [0, 1, 2, 3];
-result = arraySet(arr, 1, 0);
-//console.log(arr);
+test(
+  'Create a function addTextTo which gets two parameters: an element and a string. The function should append the given string to the content of the given element.',
+  function (assert) {
+    if (!assert.functionExists('addTextTo', ['elem', 'str'])) return;
 
-function addAll(arr, arrlen) {
-  let sum = 0;
-  for (let i = 0; i < arrlen; i++) {
-    sum += arr[i];
-    console.log(sum);
-  }
-}
+    const addTo = document.querySelector('#addto');
 
-myArray = [1, 2, 3, 4, 5, 1];
-//addAll(myArray,myArray.length);
+    assert.strictEqual(addTo.textContent, 'Does it work yet?');
 
-function larger(a, b) {
-  if (a >= b) {
-    return a;
-  } else {
-    return b;
-  }
-}
+    addTextTo(addTo, ' Maybe!');
+    assert.strictEqual(addTo.textContent, 'Does it work yet? Maybe!');
 
-//console.log(larger(7 ,6));
+    addTextTo(document.querySelector('#addto'), ' Yes!');
+    assert.strictEqual(
+      document.getElementById('addto').textContent,
+      'Does it work yet? Maybe! Yes!',
+    );
+  },
+);
 
-function compare(a, b) {
-  if (a.length !== b.length) {
-    return false;
-  }
 
-  for (i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) {
-      return false;
+test(
+  "Create a function `moreBears` that replaces the default image with the image of a bear.  Your function should (for the 'animals' element) set its source URL to 'http://placebear.com/400/200', its alternative text to 'A bear.', and its title to 'A BEAR!'.",
+  function (assert) {
+    if (!assert.functionExists('moreBears')) return;
+
+    const animals = document.querySelector('#animals');
+
+    assert.strictEqual(animals.src, 'https://dummyimage.com/600x400/FF6900/fff.png');
+    assert.strictEqual(animals.alt, 'Not a bear...');
+    assert.strictEqual(animals.title, 'Not a bear...');
+
+    moreBears();
+
+    assert.strictEqual(animals.src, 'http://placebear.com/400/200');
+    assert.strictEqual(animals.alt, 'A bear.');
+    assert.strictEqual(animals.title, 'A BEAR!');
+  },
+);
+
+
+test(
+  "Create a function `setId` with two parameters, an element and a string. The function will set the element's ID to the given string. The return value of the function must be the given element.",
+  function (assert) {
+    if (!assert.functionExists('setId', ['elem', 'str'])) return;
+
+    const p = document.createElement('p');
+    assert.notOk(p.id, 'Before running, p has no Id.');
+
+    const q = setId(p, 'lalala');
+    assert.strictEqual(p.id, 'lalala');
+    assert.strictEqual(q, p, 'The function must return its element parameter.');
+
+    setId(p, 'foobarbaz');
+    assert.strictEqual(p.id, 'foobarbaz');
+  },
+);
+
+
+test(
+  "Create a function `setClass` with two parameters, an element and a string. The function will set the element's class to the given string. The return value of the function must be the given element.",
+  function (assert) {
+    if (!assert.functionExists('setClass', ['elem', 'str'])) return;
+
+    const p = document.getElementById('setmyclass');
+    assert.notOk(p.className);
+
+    const q = setClass(p, 'banal');
+    assert.strictEqual(p.className, 'banal');
+    assert.strictEqual(q, p, 'The function must return its element parameter.');
+
+    setClass(p, 'interesting');
+    assert.strictEqual(p.className, 'interesting');
+  },
+);
+
+
+test(
+  "Create a function `addAClass` with two parameters, an element and a string. The function will add the given string's value to the element's classes. The return value of the function must be the given element.",
+  function (assert) {
+    if (!assert.functionExists('addAClass', ['elem', 'str'])) return;
+
+    let p = document.getElementById('wantaborder');
+    assert.strictEqual(p.classList.length, 1);
+    assert.strictEqual(p.className, 'greatcontent');
+
+    let q = addAClass(p, 'excellence');
+    assert.strictEqual(p.classList.length, 2);
+    assert.strictEqual(q, p, 'The function must return its element parameter.');
+    assert.ok(p.classList.contains('excellence'));
+
+    addAClass(p, 'lovely');
+    assert.strictEqual(p.classList.length, 3);
+    assert.ok(p.classList.contains('excellence'));
+    assert.ok(p.classList.contains('lovely'));
+
+    p = document.createElement('header');
+    assert.strictEqual(p.classList.length, 0);
+
+    q = addAClass(p, 'excellence');
+    assert.strictEqual(p.classList.length, 1);
+    assert.strictEqual(q, p, 'The function must return its element parameter.');
+    assert.ok(p.classList.contains('excellence'));
+  },
+);
+
+
+test(
+  "Create a function `removeAClass` with two parameters, an element and a string. The function will remove the given string's value from the element's classes. The return value of the function must be the given element.",
+  function (assert) {
+    if (!assert.functionExists('removeAClass', ['elem', 'str'])) return;
+
+    const p = document.getElementById('helpme');
+    assert.strictEqual(p.classList.length, 3);
+    assert.ok(
+      p.classList.contains('downer'),
+      'Before running, the array of classes contains `downer`.',
+    );
+
+    removeAClass(p, 'downer');
+    assert.notOk(p.classList.contains('downer'));
+    assert.strictEqual(p.classList.length, 2);
+  },
+);
+
+
+test(
+  'Write a function `newElement` which, given one parameter called name, returns a newly created element with that tag name.',
+  function (assert) {
+    if (!assert.functionExists('newElement', ['name'])) return;
+
+    let x = newElement('li');
+    assert.strictEqual(x.parentElement, null);
+    assert.strictEqual(x.nodeName, 'LI');
+
+    x = newElement('p');
+    assert.strictEqual(x.parentElement, null);
+    assert.strictEqual(x.nodeName, 'P');
+  },
+
+);
+
+
+test(
+  'Create a function `findElementById` which, given one parameter called id, returns the element that has that id.',
+  function (assert) {
+    if (!assert.functionExists('findElementById', ['id'])) return;
+
+    const thisisaparagraph = document.querySelector('#thisisaparagraph');
+    const selectus = document.querySelector('#selectus');
+
+    assert.strictEqual(
+      findElementById('thisisaparagraph'),
+      thisisaparagraph,
+    );
+
+    assert.strictEqual(
+      findElementById('selectus'),
+      selectus,
+    );
+
+    assert.equal(
+      findElementById('mustnotexist'),
+      null,
+    );
+  },
+
+);
+
+
+test(
+  'Create a function `findElementsByQuery` which, given one parameter called query, returns all the elements that match the query selector in that parameter.',
+  function (assert) {
+    if (!assert.functionExists('findElementsByQuery', ['query'])) return;
+
+    const thisisaparagraph = document.querySelector('#thisisaparagraph');
+    const selectus = document.querySelector('#selectus');
+
+    assert.deepEqual(
+      arr(findElementsByQuery('#thisisaparagraph')),
+      [thisisaparagraph],
+    );
+
+    assert.deepEqual(
+      arr(findElementsByQuery('#selectus *')),
+      arr(selectus.children),
+    );
+
+    assert.deepEqual(
+      arr(findElementsByQuery('.selectus')),
+      arr(selectus.getElementsByTagName('P')),
+    );
+
+    assert.deepEqual(
+      arr(findElementsByQuery('mustnotexist')), [],
+    );
+  },
+);
+
+
+test(
+  'Create a function `reverseList` that can reverse the content of a list (a UL or OL element).  The function should take one parameter, which is a query selector used to find the list whose children should be reversed.  Return the selected element.',
+  function (assert) {
+    if (!assert.functionExists('reverseList', ['query'])) return;
+
+    const reverseme = document.querySelector('#reverseme');
+    const reversemetoo = document.querySelector('#reversemetoo');
+
+    assert.strictEqual(
+      reverseme.firstElementChild.textContent,
+      '1',
+      'Before running, first element is 1.',
+    );
+
+    reverseList('#reverseme');
+
+    assert.strictEqual(
+      reverseme.firstElementChild.textContent,
+      '5',
+      'After running, first element must be 5.',
+    );
+
+    assert.strictEqual(
+      reverseme.lastElementChild.textContent,
+      '1',
+      'After running, last element must be 1.',
+    );
+
+    // reverse another list
+    const returnedList = reverseList('#reversemetoo');
+
+    assert.strictEqual(
+      reversemetoo.firstElementChild.textContent,
+      'The Force Awakens',
+      'After running again, first element is The Force Awakens.',
+    );
+
+    assert.strictEqual(
+      returnedList,
+      reversemetoo,
+      'The selected element is returned.',
+    );
+  },
+);
+
+
+test(
+  'Create a function `mover` that accepts two parameters, the first is a selector for the element to move, the second is a selector for the element that it should be appended to.',
+  function (assert) {
+    if (!assert.functionExists('mover', ['moveThis', 'appendToThis'])) return;
+
+    const moveme = document.querySelector('#moveme');
+    const listone = document.querySelector('#listone');
+    const listtwo = document.querySelector('#listtwo');
+
+
+    assert.ok(
+      moveme.parentElement === listone,
+      'Before, `moveme` has `listone` as a parent.',
+    );
+
+
+    assert.strictEqual(
+      listone.children.length,
+      4,
+      'Before running, list one has 4 elements.',
+    );
+
+    assert.strictEqual(
+      listtwo.children.length,
+      3,
+      'Before running, list two has 3 elements.',
+    );
+
+    mover('#moveme', '#listtwo');
+
+    assert.strictEqual(
+      listone.children.length,
+      3,
+      'After running, list one has 3 elements.',
+    );
+
+    assert.strictEqual(
+      listtwo.children.length,
+      4,
+      'After running, list two has 4 elements.',
+    );
+
+    assert.ok(
+      moveme.parentElement === listtwo,
+      'After, moveme has a new parent.',
+    );
+
+    mover('#moveme', '#listone');
+
+    assert.strictEqual(
+      listone.children.length,
+      4,
+      'After running again, list one has 4 elements.',
+    );
+
+    assert.strictEqual(
+      listtwo.children.length,
+      3,
+      'Before running again, list one has 3 elements.',
+    );
+
+
+    mover('#moveme', '#listone');
+
+    assert.strictEqual(
+      listone.children.length,
+      4,
+      'After running, list one has 4 elements.',
+    );
+
+    assert.strictEqual(
+      listtwo.children.length,
+      3,
+      'After running, list two has 3 elements.',
+    );
+
+    assert.ok(
+      moveme.parentElement === listone,
+      'After, moveme has a new parent.',
+    );
+  },
+);
+
+
+test(
+  'Write a function `filler` that accepts two parameters, the first is a list element that should be added to, the second is an array of candidate strings that will be turned into list items.',
+  function (assert) {
+    if (!assert.functionExists('filler', ['list', 'candidates'])) return;
+
+    const ul = document.getElementById('fillthislist');
+
+    const entries = ['Shmi', 'Anakin', 'Luke'];
+
+    filler(ul, entries);
+
+    for (let i = 0; i < ul.children.length; i++) {
+      assert.strictEqual(
+        ul.children[i].tagName,
+        'LI',
+      );
+      assert.strictEqual(
+        ul.children[i].textContent,
+        entries[i],
+        'Text in each element should match the text in the array.',
+      );
     }
-  }
 
-  return true;
-}
+    assert.strictEqual(
+      ul.children.length,
+      3,
+      'There should be exactly three list items.',
+    );
 
-const arr1 = [1, 2, 5];
-const arr2 = [1, 2, 5];
 
-//console.log(compare(arr1, arr2));
+    const unshowntest = document.createElement('ul');
+    filler(unshowntest, []);
+    assert.strictEqual(
+      unshowntest.children.length,
+      0,
+      'There should be exactly zero list items if called with an empty array.',
+    );
 
-function addToAll(arr, n) {
-      
-  return arr.map(element => element + n);
-  
-}
+    filler(unshowntest, ['Sloane', 'Kyrell', 'Wexley', 'Rax']);
+    assert.strictEqual(
+      unshowntest.children.length,
+      4,
+      'There should be exactly four list items if called with an array of four items.',
+    );
+  },
+);
 
-const arrayForaddToAll = [1, 2, 3];
-const number = 1;
-//console.log(addToAll(arrayForaddToAll, number));
 
-let remembered;
-function rememberThis(keepSafe) {
-  remembered = keepSafe;
-}
-rememberThis('apple');
-//console.log(remembered);
+test(
+  'Write a function `dupe` that accepts one parameter, a selector.  The function should duplicate the element chosen by the selector (and any children of it) and append it to its parent.',
+  function (assert) {
+    if (!assert.functionExists('dupe', ['selector'])) return;
 
-rememberThis('banana');
-//console.log(remembered);
+    const thingToBeDuped = document.querySelector('#dupeme');
 
-function nArray(n) {
-      emptyArray = [];
-      for (i=1; i<=n; i++){
-        emptyArray.push(i);
-      }  
-      return emptyArray;
-}
+    assert.strictEqual(
+      thingToBeDuped.parentElement.children.length,
+      2,
+      'The parent of #dupeme should have just two children before the test run, a heading and #dupeme.',
+    );
 
-//console.log(nArray(5));
+    dupe('#dupeme');
 
-function addAllOpt(arr) {
-  let arraySum = 0
-  if (arr.length == 0) {
-    return arraySum;
-  }
+    assert.strictEqual(
+      thingToBeDuped.parentElement.children.length,
+      3,
+      'The parent of #dupeme should have three children after the test run, a heading and #dupeme AND the duplicate.',
+    );
 
-  for (i = 0; i < arr.length; i++) {
-    arraySum += arr[i];
-  }
-  return arraySum;
-}
+    const paras = thingToBeDuped.parentElement.querySelectorAll('p');
 
-const arrayAddAllOpt = [1, 5];
-//console.log(addAllOpt(arrayAddAllOpt));
+    assert.strictEqual(
+      paras.length,
+      2,
+      'There should be two paras after the test.',
+    );
 
-function divisors(arr, div) {
-  const newArray = [];
-  const newArrayTwo = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % div == 0) {
+    assert.strictEqual(
+      paras[0].textContent,
+      paras[1].textContent,
+      'The two paragraphs should have the same text.',
+    );
+  },
+);
 
-      newArray.push(arr[i]);
 
-    } else {
-      newArrayTwo.push(arr[i]);
+test(
+  'Write a function `removeAll` that accepts one parameter, a selector.  All nodes that match that selector should be removed.',
+  function (assert) {
+    if (!assert.functionExists('removeAll', ['selector'])) return;
+
+    removeAll('#noevens p:nth-child(odd)');
+
+    const paras = document.querySelectorAll('#noevens p');
+
+    assert.strictEqual(
+      paras.length,
+      3,
+      'There should be three paras after the test.',
+    );
+
+    for (let i = 0; i < paras.length; i++) {
+      assert.strictEqual(
+        paras[i].textContent,
+        'I love you',
+        'The remaining text should only say I love you.',
+      );
     }
-  
-  }
-  return {
-    divisible: newArray,
-    nonDivisible: newArrayTwo
-  };
-}
+  },
+);
 
-const arrForDivisors = [1, 2, 3, 4, 5, 16];
-const div = 4;
 
-const{divisible, nonDivisible} = (divisors(arrForDivisors, div));
-//console.log('divisible array is :', divisible);
-//console.log('nonDivisible is :', nonDivisible);
+test(
+  "Write a function `getUserData` that returns an object with information from the #username, #speed, #student form input fields. The result should be an object like this: { name: 'john', speed: 30, student: true }.",
+  function (assert) {
+    if (!assert.functionExists('getUserData')) return;
 
-function multiples (n, m){
-  let myArray = [];
-  for (i=1; i<=n; i++){
-    myArray.push(i*m);
-  }
-  return myArray;
-}
+    let user = getUserData();
 
-console.log(multiples(4, 4))
+    assert.deepEqual(
+      user, {
+        name: 'John',
+        speed: 70,
+        student: true,
+      },
+    );
 
+    document.querySelector('#username').value = 'Jane';
+    document.querySelector('#speed').value = 100;
+    document.querySelector('#student').checked = false;
+
+    user = getUserData();
+
+    assert.deepEqual(
+      user, {
+        name: 'Jane',
+        speed: 100,
+        student: false,
+      },
+    );
+  },
+);
---
+++ week 2.2/index.js
@@ -0,0 +1,106 @@
+'use strict';
+/**
+ * Add your functions here...
+ */
+
+function targetTextToConsole(e) {
+    const out = e.target.textContent;
+    console.log(out);
+};
+
+function tttcAttacher() {
+    myButton = document.querySelector('#button0');
+    myButton.addEventListener('click', targetTextToConsole);
+};
+
+tttcAttacher();
+
+function lovelyToggle() {
+    // Get the paragraph element
+    var para = document.getElementById('thisisalovelyparagraph');
+
+    // Check the current color of the paragraph text
+    if (para.style.color == 'red') {
+        // If the color is currently red, change it to blue
+        para.style.color = 'blue';
+    } else {
+        // If the color is not currently red (including if it's not set at all), change it to red
+        para.style.color = 'red';
+    }
+}
+
+
+function lovelyParaAttacher() {
+    
+    const button_1 = document.querySelector('#thisisalovelyparagraph');
+    button_1.addEventListener('click', lovelyToggle);
+};
+
+const inputOne = document.querySelector("#in1");
+const inputTwo = document.querySelector("#in2");
+const out = document.querySelector("#out1");
+
+function updateOutPut() {
+
+  const combainedValue = `${inputOne?.value} ${inputTwo?.value}`;
+  out.textContent = combainedValue;
+}
+
+function checkForEnter(e) {
+  if (e.key === "Enter") {
+    updateOutPut();
+  }
+}
+
+function concatAttacher() {
+  
+  inputOne.addEventListener("keydown", checkForEnter);
+  inputTwo.addEventListener("keydown", checkForEnter);
+}
+ 
+
+
+function mouseIN(e) {
+    e.target.textContent = 'In'; 
+};
+
+function mouseOut(e) {
+  e.target.textContent = 'Out'; 
+};
+
+const mouseBox = document.querySelector('#snitch');
+mouseBox.addEventListener('mouseover', mouseIN);
+mouseBox.addEventListener('mouseout', mouseOut);
+
+function mouseIN(e) {
+    const myReport = document.querySelector('#report');
+    const x = e.clientX;
+    const y = e.clientY;
+    myReport.textContent = `x: ${x}, y: ${y}`;
+};
+
+function reportAttacher() {
+  const reportInput = document.querySelector('#mousereporter');
+  reportInput.addEventListener('mousemove',mouseIN);
+};
+
+reportAttacher();
+
+// Select the input field and the error message
+const inputField = document.querySelector('#newid');
+const errorMessage = document.querySelector('#newiderror');
+
+// Hide the error message initially
+errorMessage.style.display = 'none';
+
+// Add an input event listener to the input field
+inputField.addEventListener('input', function() {
+    // Check if the input value contains a space
+    if (inputField.value.includes(' ')) {
+        // If it does, show the error message
+        errorMessage.style.display = 'inline';
+    } else {
+        // If it doesn't, hide the error message
+        errorMessage.style.display = 'none';
+    }
+});
